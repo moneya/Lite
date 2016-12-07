b32230cf5948c2c200d48af945def9fab3ca5011
Mcdonald <davinchecodes@gmail.com>
2016-12-07 12:00:07 +0000

    admob

--- www/js/app.js
+++ www/js/app.js
@@ -38,21 +38,36 @@ angular.module('your_app_name', [
      StatusBar.styleDefault();
      }
      
-if( ionic.Platform.isAndroid() )  { 
-        admobid = { // for Android
-          banner: 'ca-app-pub-4778243441447061/9718271034'
-        };
+     //======admob code start=============
  
-        if(AdMob) 
-          AdMob.createBanner( 
-            {
-              adId:admobid.banner, 
-              position:AdMob.AD_POSITION.BOTTOM_CENTER, 
-              autoShow:true
-            } 
-          );
-    }
-    
+      var admobid = {};
+        // select the right Ad Id according to platform
+        if( /(android)/i.test(navigator.userAgent) ) { 
+            admobid = { // for Android
+                banner: 'ca-app-pub-6869992474017983/9375997553',
+                interstitial: 'ca-app-pub-6869992474017983/1657046752'
+            };
+        } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+            admobid = { // for iOS
+                banner: 'ca-app-pub-6869992474017983/4806197152',
+                interstitial: 'ca-app-pub-6869992474017983/7563979554'
+            };
+        } else {
+            admobid = { // for Windows Phone
+                banner: 'ca-app-pub-6869992474017983/8878394753',
+                interstitial: 'ca-app-pub-6869992474017983/1355127956'
+            };
+        }
+ 
+  if(window.AdMob) AdMob.createBanner( {
+      adId:admobid.banner, 
+      position:AdMob.AD_POSITION.BOTTOM_CENTER, 
+      autoShow:true} );
+ 
+//=======AdMob Code End=======
+ 
+});
+     
      PushNotificationsService.register();
      });
 
